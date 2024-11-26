996d7b5c952f12abf8d492830fc4338422adc31c
Stéphane HEURTAUX <sheurtaux@isagri.fr>
2024-11-26 12:44:22 +0100

    Refactor main.js to switch from global to local component registration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+// Mode global
+// import HomePage from './components/HomePage.vue';
+
+// const app = createApp(App).mount('#app');
+// app.component('HomePage', HomePage);
+
+// Mode local
 createApp(App).mount('#app');
