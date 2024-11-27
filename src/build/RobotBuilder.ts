981ad2b40b390b37a96276651acfd6accb04791a
Stéphane HEURTAUX <SHEURTAUX@groupeisagri.com>
2024-11-27 21:06:31 +0100

    Enhance RobotBuilder component with dynamic part selection and navigation methods

--- src/build/RobotBuilder.ts
+++ src/build/RobotBuilder.ts
@@ -1,10 +1,88 @@
 import parts from '../data/parts';
 
+function getNextValidIndex(index, length) {
+  const incrementedIndex = index + 1;
+  return incrementedIndex > length - 1 ? 0 : incrementedIndex;
+}
+
+function getPrevioustValidIndex(index, length) {
+  const deprecateddIndex = index - 1;
+  return deprecateddIndex < 0 ? length - 1 : deprecateddIndex;
+}
+
 export default {
   name: 'RobotBuilder',
   data() {
     return {
       availableParts: parts,
+      selectedHeadIndex: 0,
+      selectedLeftArmIndex: 0,
+      selectedTorsoIndex: 0,
+      selectedRightArmIndex: 0,
+      selectedBaseIndex: 0,
     };
+  },
+  methods: {
+    selectNextHead() {
+      this.selectedHeadIndex = getNextValidIndex(
+        this.selectedHeadIndex,
+        this.availableParts.heads.length,
+      );
+    },
+    selectPreviousHead() {
+      this.selectedHeadIndex = getPrevioustValidIndex(
+        this.selectedHeadIndex,
+        this.availableParts.heads.length,
+      );
+    },
+    selectNextLeftArm() {
+      this.selectedLeftArmIndex = getNextValidIndex(
+        this.selectedLeftArmIndex,
+        this.availableParts.arms.length,
+      );
+    },
+    selectPreviousLeftArm() {
+      this.selectedLeftArmIndex = getPrevioustValidIndex(
+        this.selectedLeftArmIndex,
+        this.availableParts.arms.length,
+      );
+    },
+    selectNextTorso() {
+      this.selectedTorsoIndex = getNextValidIndex(
+        this.selectedTorsoIndex,
+        this.availableParts.torsos.length,
+      );
+    },
+    selectPreviousTorso() {
+      this.selectedTorsoIndex = getPrevioustValidIndex(
+        this.selectedTorsoIndex,
+        this.availableParts.torsos.length,
+      );
+    },
+    selectNextRightArm() {
+      this.selectedRightArmIndex = getNextValidIndex(
+        this.selectedRightArmIndex,
+        this.availableParts.arms.length,
+      );
+    },
+    selectPreviousRightArm() {
+      this.selectedRightArmIndex = getPrevioustValidIndex(
+        this.selectedRightArmIndex,
+        this.availableParts.arms.length,
+      );
+    },
+    selectNextBase() {
+      this.selectedBaseIndex = getNextValidIndex(
+        this.selectedBaseIndex,
+        this.availableParts.bases.length,
+      );
+    },
+    selectPreviousBase() {
+      this.selectedBaseIndex = getPrevioustValidIndex(
+        this.selectedBaseIndex,
+        this.availableParts.bases.length,
+      );
+    },
+
   },
 };
